affcaabf284f929bc8ad0ea8aa6f1434e0246fc1
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2023-03-22 00:02:24 -0300

    feat: foi feito o filtro atráves de metodos como o filter e gets e sets  e usado o ngModel

--- Frontend/ProEventos-App/src/app/app.module.ts
+++ Frontend/ProEventos-App/src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CollapseModule } from 'ngx-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, EventosComponent, PalestrantesComponent, NavbarComponent],
-  imports: [BrowserModule ,AppRoutingModule, HttpClientModule, BrowserAnimationsModule, CollapseModule.forRoot() ],
+  imports: [BrowserModule ,AppRoutingModule, HttpClientModule, BrowserAnimationsModule, CollapseModule.forRoot(),FormsModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- Frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { EventosService } from 'src/services/eventos.service';
 import { Event } from '../models/events.model';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-eventos',
@@ -10,25 +11,48 @@ import { Event } from '../models/events.model';
 })
 export class EventosComponent implements OnInit {
   events?: Event[];
-  widhtImage:number = 50;
+  eventFilter: any;
+  widhtImage: number = 50;
   heightImage: number = 110;
   marginImage: number = 5;
   showImage: boolean = true;
+  private _filtroLista: string = '';
 
   constructor(private event: EventosService) {}
 
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventFilter = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.events;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return (this.events = this.events?.filter(
+      (event: any) =>
+        event.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        event.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    ));
+  }
+
   ngOnInit(): void {
-    this.getEventos();
+    this.getEvents();
   }
 
-  ShowImage(){
-    this.showImage = !this.showImage
+  ShowImage() {
+    this.showImage = !this.showImage;
   }
 
-  getEventos() {
+  getEvents() {
     this.event.getEventosApi().subscribe((res: any) => {
+      this.eventFilter = res;
       this.events = res;
-
     });
   }
 }
